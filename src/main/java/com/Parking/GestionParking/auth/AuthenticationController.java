3f95d094dd4141dc66a6d1aa77773282d5b03990
yosserchbinou <yosser.chbinou@esprit.tn>
2024-10-31 20:24:30 +0100

    register

--- src/main/java/com/Parking/GestionParking/auth/AuthenticationController.java
+++ src/main/java/com/Parking/GestionParking/auth/AuthenticationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://192.168.40.149:4200")
 @Tag(name= "Authentication")
 
 public class AuthenticationController {
