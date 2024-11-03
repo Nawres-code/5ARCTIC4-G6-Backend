1f96d60cc47d4a8e1a0edb55e4776f5cb351e923
nawres lakhal <nawres.lakhal2000@gmail.com>
2024-11-03 15:22:54 +0100

    test

---
+++ src/test/java/com/Parking/GestionParking/GestionParkingTest.java
@@ -0,0 +1,13 @@
+package com.Parking.GestionParking;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GestionParkingTest {
+
+    @Test
+    public void testExample() {
+        assertTrue(true);  // Always passes, but it's a valid test
+    }
+}
