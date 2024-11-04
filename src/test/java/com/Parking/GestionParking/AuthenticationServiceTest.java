6ec20afebcaed3a433fe58610e95b6c01335969e
Salsabyl Ham <salsabilhamraoui510@gmail.com>
2024-11-04 19:29:13 +0100

    yaaala

---
+++ src/test/java/com/Parking/GestionParking/AuthenticationServiceTest.java
@@ -0,0 +1,13 @@
+package com.Parking.GestionParking;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AuthenticationServiceTest {
+
+    @Test
+    void testExample() {
+        // Test simple pour vérifier que le test fonctionne
+        assertTrue(true);
+    }
+}
